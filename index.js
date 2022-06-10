ddc90bf2f2b95dfb9716e22464c760ad587e9d8e
GLADYS <mungaiglado106@gmail.com>
2022-06-10 17:06:06 +0300

    commit tree ee555fe2beb462ca0e61c6a68e712cf311be083d

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
-// Write your solution here!
+const cats = ["Milo","Otis","Garfield"];
+
+function destructivelyAppendCat (name) {
+    cats.push(name);
+}
+
+function destructivelyAppendDriver (name) {
+    drivers.push(name);
+}
+function destructivelyRemoveLastDriver () {
+    drivers.pop();
+}
+
+
+function destructivelyPrependDriver (name) {
+    drivers.unshift(name);
+}
+  function destructivelyPrependCat (name) {
+    cats.unshift(name);
+  }  
+
+  function removeLastCat () {
+    return cats.slice(0, cats.length - 1);
+  }
+  function destructivelyRemoveFirstCat () {
+    cats.shift();
+  }
+   function destructivelyRemoveLastCat () {
+    cats.pop();
+   }
+  
+  function removeLastDriver () {
+    return drivers.slice(0, drivers.length - 1);
+  }
+
+  function removeFirstDriver () {
+    return drivers.slice(1);
+  }
+  function removeFirstCat (){
+    return cats.slice(1);
+  }  
+  
+function appendCat (name) {
+    return [...cats, name];
+}
+function appendDriver (name) {
+    return [...drivers, name];
+}
+function prependDriver (name) {
+    return [name, ...drivers];
+}
+  function prependCat (name) {
+    return [name, ...cats];
+  }
+
+  
